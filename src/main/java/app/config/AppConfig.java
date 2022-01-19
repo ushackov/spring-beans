1dc4e0e4b240f6b95fd6cd9d2c425452d7a97cf4
ushackov <ushackov@gmail,com>
2022-01-20 00:50:46 +0300

    Узнал что при связывании по типу нужно указывать имя.

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,19 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
